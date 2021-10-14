900d67ffa3cf62ceb5ce4077e001ebdf5b320004
KuzminOleg2pc2 <91515428+Ugwey2021@users.noreply.github.com>
2021-10-14 10:52:44 +0500

    Update Program.cs

--- pz_8/Program.cs
+++ pz_8/Program.cs
@@ -9,14 +9,14 @@ namespace pz_8
             Random rnd = new Random();
             int[] a =  new int[10];
             Console.Write("Исходный массив");
-            for(int i=0; i < 10; i++)
+            for(int i=0; i < 10; i++)//Заполняем массив случайными цифрами
             {
                 a[i] = rnd.Next(-10, 11);
                 Console.Write(" "+a[i]);
             }
             Console.WriteLine();
             Console.Write("Результат");
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 10; i++)// делаем сдвиг
             {
                 if(i != 9) a[i] = a[i + 1];
                 else a[i] = 0;
